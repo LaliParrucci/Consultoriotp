1b5a51a7e80bfb1d4a0407eb4262576b7fb183d6
LaliParrucci <68954029+LaliParrucci@users.noreply.github.com>
2020-10-12 15:46:12 -0300

    Reporte practicas modificado

--- GUILayer/ReporteListadoPracticas.Designer.cs
+++ GUILayer/ReporteListadoPracticas.Designer.cs
@@ -30,6 +30,17 @@
         {
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.gpxFiltrosPracticas = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gpxFiltrosPracticas.SuspendLayout();
             this.SuspendLayout();
             // 
             // reportViewer1
@@ -44,12 +55,105 @@
             // 
             // gpxFiltrosPracticas
             // 
+            this.gpxFiltrosPracticas.Controls.Add(this.button1);
+            this.gpxFiltrosPracticas.Controls.Add(this.textBox4);
+            this.gpxFiltrosPracticas.Controls.Add(this.textBox3);
+            this.gpxFiltrosPracticas.Controls.Add(this.label4);
+            this.gpxFiltrosPracticas.Controls.Add(this.label3);
+            this.gpxFiltrosPracticas.Controls.Add(this.label2);
+            this.gpxFiltrosPracticas.Controls.Add(this.textBox2);
+            this.gpxFiltrosPracticas.Controls.Add(this.label1);
+            this.gpxFiltrosPracticas.Controls.Add(this.maskedTextBox1);
+            this.gpxFiltrosPracticas.Controls.Add(this.textBox1);
             this.gpxFiltrosPracticas.Location = new System.Drawing.Point(12, 12);
             this.gpxFiltrosPracticas.Name = "gpxFiltrosPracticas";
             this.gpxFiltrosPracticas.Size = new System.Drawing.Size(970, 201);
             this.gpxFiltrosPracticas.TabIndex = 2;
             this.gpxFiltrosPracticas.TabStop = false;
             this.gpxFiltrosPracticas.Text = "Filtros";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(164, 45);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(8, 26);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(244, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Id Practica:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Importe desde:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(426, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Importe hasta:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(559, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(244, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(559, 90);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(244, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(660, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listadoPracticas
             // 
@@ -62,6 +166,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Listado de Practicas";
             this.Load += new System.EventHandler(this.listadoPracticas_Load);
+            this.gpxFiltrosPracticas.ResumeLayout(false);
+            this.gpxFiltrosPracticas.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,5 +175,15 @@
         #endregion
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
         private System.Windows.Forms.GroupBox gpxFiltrosPracticas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- GUILayer/ReporteListadoPracticas.cs
+++ GUILayer/ReporteListadoPracticas.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 using Consultorio.DataAccessLayer;
 using Microsoft.Reporting.WinForms;
 using Consultorio.BussinessLayer;
+using Consultorio.Entities;
 
 namespace Consultorio.GUILayer
 {
@@ -24,22 +25,74 @@ namespace Consultorio.GUILayer
         private void listadoPracticas_Load(object sender, EventArgs e)
         {
 
-            this.reportViewer1.RefreshReport();
+
         }
 
         private void reportViewer1_Load(object sender, EventArgs e)
         {
-            DataTable tabla = new DataTable();
-            tabla = practicas.recuPracticas();
 
-            ReportDataSource rp = new ReportDataSource("DatosPracticas",tabla);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            DataTable tabla = new DataTable();
+            tabla = generador();
+
+            ReportDataSource rp = new ReportDataSource("DatosPracticas", tabla);
 
             reportViewer1.LocalReport.DataSources.Clear();
             reportViewer1.LocalReport.DataSources.Add(rp);
             reportViewer1.LocalReport.Refresh();
 
+            this.reportViewer1.RefreshReport();
 
+        }
+        private DataTable generador()
+        {
+            String msg = "";
+            if (textBox1.Text != "")
+            {
+                msg = msg + " AND nombre LIKE '%" + textBox1.Text + "%'";
+            }
+            if (textBox2.Text != "")
+            {
+                msg = msg + " AND id_practica = '" + textBox2.Text + "'";
+            }
+            if (textBox3.Text != "")
+            {
+                msg = msg + " AND importe >= " + textBox3.Text + "";
+            }
+            if (textBox4.Text != "")
+            {
+                msg = msg + " AND importe <= " + textBox4.Text + "";
+            }
+            
+            FiltrosReportePracticas filtros = new FiltrosReportePracticas ();
+            if (msg != "")
+            {
+  
+             msg = msg.Remove(0, 5);
+             return filtros.ObtenerListadoPracticasFiltro(msg);
+           
+
+            }
+            
+             return filtros.ObtenerListadoPracticas();
             
         }
     }
+
 }
